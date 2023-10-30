bcd273c40014e33a26e66ae452f9505490beaeb4
pinipaper <pinipaper@gmail.com>
2023-10-30 17:18:46 +0900

    chore: rename files
    - add mqtt5 example code
    - rename wifi to wifi_cs
    - change project name

--- src/esp32/main/main.c
+++ src/esp32/main/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
-#include "components/wifi/include/wifi.h"
+#include <esp_log.h>
+#include <esp_chip_info.h>
+#include <esp_system.h>
+#include <esp_flash.h>
+#include <nvs_flash.h>
 
-void app_main(void)
-{
-    // TODO: Implement nvs init
+const char *TAG = "main";
+
+void __initialize_prequisites(void) {
+    esp_err_t nvs_ret = nvs_flash_init();
+    if (nvs_ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        nvs_ret == ESP_ERR_NVS_NEW_VERSION_FOUND
+    ) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());    
+    }
+   
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_LOGI(TAG, "netif initialized");
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_LOGI(TAG, "event loop created");
+
     wifi_init_sta();
+    ESP_LOGI(TAG, "wifi initialized");
+
+    ESP_LOGI(TAG, "prequisites initialized");
+    return;
+}
+
+void app_main(void) {
+    ESP_LOGI(TAG, "program started");
+    __initialize_prequisites();
 }
